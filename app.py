6b007b2ed1529c2836367fb7d64e4b067a485780
Andru Cupala <andru@cupala.com>
2025-11-17 12:49:24 -0800

    Created base code that shows a window.

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
-class App():
-    def run(self):
+import panda2d
+
+class App(panda2d.PandaApp):
+    def initialize(self):
+        pass
+    
+    def update(self, dt):
+        pass
+    
+    def draw(self):
         pass
--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-import app
+import panda2d
+from app import App
+
+def main():
+    application = App(
+        width=800,
+        height=600,
+        title="HTG PayaLabs Game",
+        resizable=panda2d.Resizable.NONE
+    )
+    application.run()
 
 if __name__ == "__main__":
-    application = app.App()
-    application.run()
+    main()
