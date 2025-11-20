6e8f9ddb679296e3266bcdd44b33a33fb8411849
Andru Cupala <andru@cupala.com>
2025-11-20 13:46:08 +0900

    Added interface layout to app.

--- app.py
+++ app.py
@@ -4,15 +4,27 @@ import panda2d
 class App(panda2d.PandaApp):
     def initialize(self):
         self.ares_image = panda2d.Image("assets/ares.png")
+        self.test_scene = panda2d.Image("assets/test-scene.png")
+        
+        self.main_font = "assets/Khmer MN.ttc"
     
     def update(self):
         pass
     
     def draw(self):
-        self.clear((50, 50, 50))  # gray background
-        self.fill_rect(-50, -50, 50, 50, (255, 0, 0))
-        self.draw_image(self.ares_image, 0, 0, align=panda2d.Align.CENTER, anti_aliasing=False, width=self.ares_image.width * 5, height=self.ares_image.height * 5)
+        self.clear((0, 0, 0))  # Black background
 
+        # Context image
+        self.draw_image(self.test_scene, 240, 180, align=panda2d.Align.TOP_RIGHT, anti_aliasing=False)
+
+        # Contest text
+        self.draw_text(-230, 40, "Hello world!\nYou are on a plane.\nYou will crash!", (255, 255, 255),
+               align=panda2d.Align.TOP_LEFT,
+               font=self.main_font,
+               size=24, 
+               newline_spacing=20
+               )
+        
 
 def main():
     application = App(
